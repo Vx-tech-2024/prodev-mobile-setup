b2d003614fb91404a3ff83f014e80ffbb42a60a1
Vx-tech <xaviervianney8@gmail.com>
2026-01-08 17:34:07 +0300

    Tracking errors

--- prodev-mobile-app-0x02/app/index.tsx
+++ prodev-mobile-app-0x02/app/index.tsx
@@ -26,15 +26,9 @@ export default function Index() {
 
             {/* Text Group */}
             <View style={styles.textGroup}>
-              <Text style={styles.textLarge}>
-                Find your favorite place here
-              </Text>
-              <Text style={styles.textSmall}>
-                The best prices for over 2  
-              </Text>
-              <Text style={styles.textSmall}>
-                million properties worldwide
-              </Text>
+              <Text style={styles.textLarge}>Find your favorite place here</Text>
+              <Text style={styles.textSmall}>The best prices for over 2</Text>
+              <Text style={styles.textSmall}>million properties worldwide</Text>
             </View>
 
             {/* Bottom Section */}
